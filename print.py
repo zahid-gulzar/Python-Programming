9cde285bb229dde17bbf435b1aebf054ae42a074
zahid-gulzar <zahidgulzar1926@gmail.com>
2025-09-18 09:05:04 +0000

    This is about print statement

---
+++ print.py
@@ -0,0 +1,18 @@
+# print displays output to your console
+print('Hello World!')   # Basic print statement
+
+# Enclose strings in single or double quotes
+print('Hello World single quotes')
+print("Hello World double quotes")
+
+# Getting information from the user
+name = input('Please enter your name: ')   # Stores user input in variable 'name'
+print(name)  # Prints whatever the user typed
+
+# Printing blank lines can improve readability
+print('Hello World')
+print()   # Prints a blank line
+print('Did you see that blank line ?')
+
+# Using \n to insert a line break inside a string
+print('Blank line \nin the middle of string')
