7cacf0af3de6dcbc53b5b42939728078b1ffd2e9
zahid-gulzar <zahidgulzar1926@gmail.com>
2025-09-18 10:34:05 +0000

    Learn comments in python

---
+++ comments.py
@@ -0,0 +1,10 @@
+# => We add comments to our code using symbol #
+
+# This is a comment in my code it does nothing
+
+# => Lines that start with # are not executed
+
+# This is a comment in my code it does nothing
+# print('Hello world')
+# print("Hello world")
+# No output will hbe displayed !
